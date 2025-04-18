87632bc93a05652bb4c9aa45a933e7b3e95d5d79
wendwin <wendwin12@gmail.com>
2025-04-18 09:01:24 +0700

    menambahakan model payment transaction dan menamba fitur tipe pembayaran

---
+++ app/Models/PaymentTransaction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentTransaction extends Model
+{
+    protected $table = 'payment_transactions';
+    
+    protected $fillable = [
+        'booking_id',
+        'order_id',
+        'amount',
+        'payment_type',
+        'payment_for',
+        'transaction_time',
+        'status',
+        'payment_gateway',
+        'snap_token',
+        'redirect_url',
+    ];
+}
