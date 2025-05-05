b34773b08e035cf66d1d0c2cb67f4966b4a48f5b
patanpaten <mfathanalfaruq2@gmail.com>
2025-05-05 11:06:41 +0700

    Menambahkan fitur penggajiian

---
+++ database/migrations/2025_05_05_035852_create_salaries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->id('salaries_id');
+            $table->string('nama');
+            $table->string('role');
+            $table->string('no_lambung');
+            $table->float('salarie');
+            $table->float('total_salary');
+            $table->date('payment_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salaries');
+    }
+}
